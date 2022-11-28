2ed253265bb82175dcc658d2e4919336823cd48b
mothishc <109157027+mothishc@users.noreply.github.com>
2022-11-28 09:23:20 +0530

    Update MethodOverloading.java

--- JAVA/MethodOverloading.java
+++ JAVA/MethodOverloading.java
@@ -1,1 +1,17 @@
 
+class Method_overloading {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+class TestOverloading1 {
+    public static void main(String[] args) {
+        System.out.println(Method_overloading.add(99, 69));
+        System.out.println(Method_overloading.add(69, 09, 69));
+    }
+}
