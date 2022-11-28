8b7f51a8715b15c9915634deb68621a0d7019cc1
mothishc <109157027+mothishc@users.noreply.github.com>
2022-11-28 09:35:37 +0530

    Create SearchingProduct.java

---
+++ SELENIUM/SearchingProduct.java
@@ -0,0 +1,32 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Selenium_SearchProduct {
+
+    public static void main(String[] args) throws InterruptedException {
+        // TODO Auto-generated method stub
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\madhu\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("https://www.amazon.com/");
+        Thread.sleep(2000);
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"nav-main\"]/div[1]/div/div/div[3]/span[1]/span/input")).click();
+        WebElement findElement = driver.findElement(By.id("twotabsearchtextbox"));
+        findElement.click();
+        findElement.sendKeys("2022 Apple MacBook Pro Laptop with M2 chip");
+        Thread.sleep(3000);
+        driver.findElement(By.id("nav-search-submit-button")).click();
+    }
+}
